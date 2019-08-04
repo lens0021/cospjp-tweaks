668b466d874ce13967a73f5b932edbeeb1eeac23
lens0021 <lorentz0021@gmail.com>
2019-08-05 01:32:06 +0900

    Let Esc toggle view and add close button

--- scripts/view_photo.js
+++ scripts/view_photo.js
@@ -4,19 +4,44 @@ const body = document.querySelector("body");
 const imageBox = document.querySelector(".image_box");
 const imageMask = document.querySelector(".image_mask");
 const photoFooter = document.querySelector("#photo_footer");
+const closeButton = document.createElement("button");
 
-const toggleImageBox = e => {
-  e.stopPropagation();
+const toggleImageBox = () => {
   imageBox.style.visibility =
     imageBox.style.visibility == "hidden" ? "visible" : "hidden";
 };
 
-imageBox.addEventListener("click", toggleImageBox);
-body.addEventListener("click", toggleImageBox);
+document.onkeydown = e => {
+  e = e || window.event;
+  if (
+    !"key" in e ||
+    (e.key !== "Escape" && e.key !== "Esc" && e.keyCode !== 27)
+  ) {
+    return;
+  }
 
-document.querySelector("body").addEventListener("click", e => {});
+  toggleImageBox();
+};
+
+imageBox.addEventListener("click", e => {
+  e.stopPropagation();
+  toggleImageBox();
+});
+
+body.addEventListener("click", e => {
+  e.stopPropagation();
+  toggleImageBox();
+});
 
 imageMask.addEventListener("click", e => {
   e.stopPropagation();
   imageBox.classList.toggle("magnified");
+});
+
+closeButton.classList.add("close-button");
+closeButton.innerText = "×";
+body.appendChild(closeButton);
+closeButton.addEventListener("click", e => {
+  e.preventDefault();
+  window.close();
 });
