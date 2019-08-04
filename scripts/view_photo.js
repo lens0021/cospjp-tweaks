118b6ee05394be5286ff246c923b7dc7fd4115d4
lens0021 <lorentz0021@gmail.com>
2019-08-04 20:55:53 +0900

    Modify view_photo style

--- scripts/view_photo.js
+++ scripts/view_photo.js
@@ -1,5 +1,19 @@
 "strict";
 
-document.querySelector(".image_box").addEventListener("click", e => {
-  window.close();
+const body = document.querySelector("body");
+const imageBox = document.querySelector(".image_box");
+const photoFooter = document.querySelector("#photo_footer");
+
+const toggleImageBox = e => {
+  e.stopPropagation();
+  imageBox.style.visibility =
+    imageBox.style.visibility == "hidden" ? "visible" : "hidden";
+  console.log("visibility: ", imageBox.style.visibility);
+};
+
+imageBox.addEventListener("click", toggleImageBox);
+body.addEventListener("click", toggleImageBox);
+
+document.querySelector("body").addEventListener("click", e => {
+  console.log("test");
 });
