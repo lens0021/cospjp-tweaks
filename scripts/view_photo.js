0a2ec640aec227753d3d856587c1b9ec13bdcbbc
lens0021 <lorentz0021@gmail.com>
2019-08-05 00:15:08 +0900

    Click photo to magnify

--- scripts/view_photo.js
+++ scripts/view_photo.js
@@ -2,18 +2,21 @@
 
 const body = document.querySelector("body");
 const imageBox = document.querySelector(".image_box");
+const imageMask = document.querySelector(".image_mask");
 const photoFooter = document.querySelector("#photo_footer");
 
 const toggleImageBox = e => {
   e.stopPropagation();
   imageBox.style.visibility =
     imageBox.style.visibility == "hidden" ? "visible" : "hidden";
-  console.log("visibility: ", imageBox.style.visibility);
 };
 
 imageBox.addEventListener("click", toggleImageBox);
 body.addEventListener("click", toggleImageBox);
 
-document.querySelector("body").addEventListener("click", e => {
-  console.log("test");
+document.querySelector("body").addEventListener("click", e => {});
+
+imageMask.addEventListener("click", e => {
+  e.stopPropagation();
+  imageBox.classList.toggle("magnified");
 });
